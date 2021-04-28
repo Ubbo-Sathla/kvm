434ca95bb14b8e184d7501766b984796146f20e7
root <843592242@qq.com>
2021-04-28 07:53:04 +0000

    kvm script

---
+++ bin/kvm.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+VM_NAME=$1
+
+BASE_XML=/opt/kvm/xml/base.xml
+VM_XML=/opt/vm/xml/$VM_NAME.xml
+
+# create vm
+qemu-img create -b /opt/vm/imgs/CentOS-7-x86_64-GenericCloud-2009.qcow2 -f qcow2 /opt/vm/disks/$VM_NAME.qcow2 100G
+
+cp $BASE_XML $VM_XML
+
+sed -i "s/base-vm/$VM_NAME/g" $VM_XML
+
+virsh define  $VM_XML
+virsh start $VM_NAME
