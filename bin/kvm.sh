fc8855b9854c56edb657f4765acc28e752f35383
843592242@qq.com <843592242@qq.com>
2021-08-16 09:13:30 +0000

    update tmp

--- bin/kvm.sh
+++ bin/kvm.sh
@@ -14,17 +14,18 @@ VM_IMG=/opt/vm/disks/$VM_NAME.qcow2
 # BASE_IMG=/opt/vm/imgs/rocky_x86_64.docker.qcow2
 BASE_IMG=/opt/vm/imgs/CentOS-7-x86_64-GenericCloud-2009.qcow2
 
+CLOUD_CONFIG=/tmp/network_config_static.cfg.tmp
 CLOUD_INIT=/tmp/$VM_NAME-cloud-init.iso
 
 # create vm
 # qemu-img create -b /opt/vm/imgs/CentOS-7-x86_64-GenericCloud-2009.qcow2 -f qcow2 /opt/vm/disks/$VM_NAME.qcow2 100G
 qemu-img create -b $BASE_IMG  -f qcow2 $VM_IMG 100G
 
-cp /opt/kvm/cloud-init/network_config_static.cfg /tmp/network_config_static.cfg.tmp
+cp /opt/kvm/cloud-init/network_config_static.cfg $CLOUD_CONFIG
 
-sed -i "s#IP#$VM_IP#g" /opt/kvm/cloud-init/network_config_static.cfg.tmp
+sed -i "s#IP#$VM_IP#g" $CLOUD_CONFIG
 
-cloud-localds -v --network-config=/tmp/network_config_static.cfg.tmp $CLOUD_INIT   /opt/kvm/cloud-init/cloud.cfg
+cloud-localds -v --network-config=$CLOUD_CONFIG $CLOUD_INIT   /opt/kvm/cloud-init/cloud.cfg
 
 cp $BASE_XML $VM_XML
 
