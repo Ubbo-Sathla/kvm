458169f2ab9d6b372899913dd2a38f6eb08bc0ab
843592242@qq.com <843592242@qq.com>
2021-08-16 08:53:09 +0000

    all

--- bin/kvm.sh
+++ bin/kvm.sh
@@ -1,29 +1,36 @@
 #!/bin/bash
 
 VM_NAME=$1
+VM_IP=$2
+VM_CPU=$3
+VM_MEM=$4
 
 BASE_XML=/opt/kvm/xml/ovs-base.xml
 VM_XML=/opt/vm/xml/$VM_NAME.xml
 
 VM_IMG=/opt/vm/disks/$VM_NAME.qcow2
 
-BASE_IMG=/opt/vm/imgs/rocky_x86_64.qcow2
+# BASE_IMG=/opt/vm/imgs/rocky_x86_64.qcow2
+# BASE_IMG=/opt/vm/imgs/rocky_x86_64.docker.qcow2
+BASE_IMG=/opt/vm/imgs/CentOS-7-x86_64-GenericCloud-2009.qcow2
 
-CLOUD_INIT=/opt/vm/cloud-init/cloud-init.iso
+CLOUD_INIT=/tmp/$VM_NAME-cloud-init.iso
 
 # create vm
 # qemu-img create -b /opt/vm/imgs/CentOS-7-x86_64-GenericCloud-2009.qcow2 -f qcow2 /opt/vm/disks/$VM_NAME.qcow2 100G
 qemu-img create -b $BASE_IMG  -f qcow2 $VM_IMG 100G
 
-cp /opt/kvm/cloud-init/network_config_static.cfg /opt/kvm/cloud-init/network_config_static.cfg.tmp
+cp /opt/kvm/cloud-init/network_config_static.cfg /tmp/network_config_static.cfg.tmp
 
-sed -i "s#IP#$2#g" /opt/kvm/cloud-init/network_config_static.cfg.tmp
+sed -i "s#IP#$VM_IP#g" /opt/kvm/cloud-init/network_config_static.cfg.tmp
 
-cloud-localds -v --network-config=/opt/kvm/cloud-init/network_config_static.cfg.tmp $CLOUD_INIT   /opt/kvm/cloud-init/cloud.cfg
+cloud-localds -v --network-config=/tmp/network_config_static.cfg.tmp $CLOUD_INIT   /opt/kvm/cloud-init/cloud.cfg
 
 cp $BASE_XML $VM_XML
 
 sed -i "s#vm-name#$VM_NAME#g" $VM_XML
+sed -i "s#vm-mem#$VM_MEM#g" $VM_XML
+sed -i "s#vm-cpu#$VM_CPU#g" $VM_XML
 sed -i "s#vm-img#$VM_IMG#g" $VM_XML
 sed -i "s#base-img#$BASE_IMG#g" $VM_XML
 sed -i "s#cloud-init#$CLOUD_INIT#g" $VM_XML
